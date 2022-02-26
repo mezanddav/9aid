df7583c731e45bd6a008db06799c1e5bf4c1a89f
David Mezei <mezanddav@gmail.com>
2022-02-26 20:14:05 +0000

    Info

--- footer.php
+++ footer.php
@@ -18,6 +18,12 @@
 			<svg width="20" height="20"><use xlink:href="#9gag-logo"></use></svg>
 			<div>Created by 9GAG with ❤️</div>
 		</div>
+		<div class="footer__content">This website is maintained by the community.</div>
+		<div class="footer__help">
+			<h2 class="footer__help-title">Would you like to help?</h2>
+			<div><a href="https://discord.gg/kVX8z86Y">Discord</a></div>
+		</div>
+		<div class="footer__ver"><?= date('Y') ?> ver. 0.0.12</div>
 	</div>
 </footer>
 </div>
